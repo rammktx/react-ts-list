d23fc59cf1b6200c604c97b0d383c02e820d9d27
rammktx <123376855+rammktx@users.noreply.github.com>
2023-01-25 10:18:31 -0500

    2 files modified, 2 added

--- index.tsx
+++ index.tsx
@@ -2,13 +2,15 @@ import * as React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './App';
+import MyList from './MyList';
+
+const toDos = ['Buy ice cream', 'Eat ice cream', 'Go to the gym'];
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <MyList theList={toDos} />
   </StrictMode>
 );
